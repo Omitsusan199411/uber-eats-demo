5dc6953d9277f87f265f04022e1214916bbdda28
Omitsusan199411 <omitsusan5014@gmail.com>
2022-06-24 02:34:50 +0900

    APIリクエスト先のURL定義ファイルを作成

---
+++ frontend/src/urls/urlApi.ts
@@ -0,0 +1,19 @@
+// APIへのリスエスと先を定義
+
+const DEFALUT_API_URL = "http://localhost:3000/api/v1";
+
+//  api/v1/restaurants
+export const restaurants = `${DEFALUT_API_URL}/restaurants`;
+
+// api/v1/foods#index
+export const foodsIndex = (restaurantId: number) =>
+  `${DEFALUT_API_URL}/restaurants/${restaurantId}/foods`;
+
+// api/v1/line_foods
+export const lineFoods = `${DEFALUT_API_URL}/line_foods`;
+
+// api/v1/line_foods#replace
+export const lineFoodsReplace = `${DEFALUT_API_URL}/line_foods/replace`;
+
+// api/v1/orders
+export const orders = `${DEFALUT_API_URL}/orders`;
