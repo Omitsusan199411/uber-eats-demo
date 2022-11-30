4b5e5714ab32934b814ba599a56916c87f0dac16
Omitsusan199411 <omitsusan5014@gmail.com>
2022-12-01 02:23:41 +0900

    src/types/mui/Palette.d.tsの作成

---
+++ frontend/src/types/mui/Palette.d.ts
@@ -0,0 +1,22 @@
+export declare module '@mui/material/styles' {
+  interface Palette {
+    basis: {
+      main: string;
+      light: string;
+      sub: string;
+    };
+  }
+  interface PaletteOptions {
+    basis?: {
+      main?: string;
+      light?: string;
+      sub?: string;
+    };
+  }
+}
+
+export declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    basis: true;
+  }
+}
