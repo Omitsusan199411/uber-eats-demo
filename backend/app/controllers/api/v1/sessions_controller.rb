0160ee794f6f938f0fee0e5d63f69d6a63a153cd
Omitsusan199411 <omitsusan5014@gmail.com>
2023-04-13 04:52:51 +0900

    サインイン成功後の認証されたユーザーデータの受け渡し

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     if @user&.authenticate(session_params[:password]) # nilとfalseは「偽」、認証できれば、Userオブジェクトを返す
       # login!のレシーバはApi::V1::SessionsControllerのインスタンス(sessions_helper.rbのSessionsHelperモジュールからinclude)
       login!(@user)
-      render json: { logged_in: true, user: @user }, status: :created
+      render json: { email: @user.email, name: @user.name, sign_in_state: true }, status: :created
     else
       render json: { message: '認証に失敗しました。' }, status: :unauthorized
     end
@@ -12,15 +12,15 @@ class Api::V1::SessionsController < ApplicationController
 
   def sign_out
     reset_session
-    reder json: { logged_out: true, message: 'サインアウトしました' }, status: :ok
+    reder json: { sign_out_state: true, message: 'サインアウトしました' }, status: :ok
   end
 
   def logged_in?
     # current_userのレシーバはApi::V1::SessionsControllerのインスタンス(sessions_helper.rbのSessionsHelperモジュールからinclude)
     if current_user
-      render json: { logged_in: true, user: @current_user }
+      render json: { sign_in_state: true, user: @current_user }
     else
-      render json: { logged_in: false, message: 'ユーザーが存在しません' }
+      render json: { sign_in_state: false, message: 'ユーザーが存在しません' }
     end
   end
 
